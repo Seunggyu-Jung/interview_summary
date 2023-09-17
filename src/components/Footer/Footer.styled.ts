1ea06469760b5950efbf996d6bba78cafd052b4a
Daniel <wswm2007@naver.com>
2023-09-17 21:00:47 +0900

    feat: footer에 링크 모달창 버튼 추가 및 UI 변경

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,6 +1,46 @@
 import { styled } from 'styled-components';
 
 export const Footer = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: space-around;
+  align-items: center;
+  flex-grow: 1;
   border-top: 1px solid #eee;
-  padding: 20px 20px 0 220px;
+  padding: 10px 0;
+  background-color: #f2f2f2;
+`;
+
+export const GithubBtn = styled.img`
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  object-fit: cover;
+  padding: 0;
+`;
+
+export const modalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const modalAnchor = styled.button`
+  width: 100%;
+  padding: 20px;
+  background: none;
+  border-radius: 10px;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: antiquewhite;
+  }
 `;
