7242116dc6659cf121fc9e93d08f86f3f70fe10c
Seongeun Lee <tjsrms1227@gmail.com>
2023-09-23 19:41:52 +0900

    feat: 검색 내용 일치 결과 전체 출력

--- src/components/ResultBox/ResultBox.tsx
+++ src/components/ResultBox/ResultBox.tsx
@@ -4,11 +4,13 @@ import { useParams } from 'react-router-dom';
 import * as S from './ResultBox.styled';
 
 export default function ResultBox() {
-  const { search } = useParams();
+  const { search = '' } = useParams();
 
-  const result = data[0].items.find(item => item.topic === search);
+  const resultArr = data[0].items.filter(
+    item => item.topic.includes(search) || item.texture.includes(search)
+  );
 
-  if (!result) {
+  if (resultArr.length === 0) {
     return (
       <div>
         <p>검색하신 내용은 없는 내용입니다.</p>
@@ -17,13 +19,19 @@ export default function ResultBox() {
   }
 
   return (
-    <div>
-      <S.ResultLink to={`/detail/${result?.id}`}>{result?.topic}</S.ResultLink>
-      <S.ResultTexture>
-        {result?.texture
-          .slice(0, 300)
-          .replace(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/gi, '') + '...'}
-      </S.ResultTexture>
-    </div>
+    <>
+      {resultArr.map(result => (
+        <div>
+          <S.ResultLink to={`/detail/${result?.id}`}>
+            {result?.topic}
+          </S.ResultLink>
+          <S.ResultTexture>
+            {result?.texture
+              .slice(0, 300)
+              .replace(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/gi, '') + '...'}
+          </S.ResultTexture>
+        </div>
+      ))}
+    </>
   );
 }
